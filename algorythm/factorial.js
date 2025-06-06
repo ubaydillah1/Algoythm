e9c0ec68e57ef4fba161c96e95426e3120dc8b3f
ubaydillah1 <ubaydillah1737@gmail.com>
2025-06-07 02:53:09 +0700

    add: factorial problem

---
+++ algorythm/factorial.js
@@ -0,0 +1,9 @@
+const faktorial = (n) => {
+  if (n == 0) {
+    return 1;
+  }
+
+  return n * faktorial(n - 1);
+};
+
+console.log(faktorial(5));
---
+++ algorythm/fibonacciSequence.js
@@ -0,0 +1,49 @@
+// me version
+function fibonacci1(n) {
+  if (n < 1) return [];
+  if (n == 1) return [0];
+
+  const result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    const resultLength = result.length;
+    const newNumber = result[resultLength - 2] + result[resultLength - 1];
+    result.push(newNumber);
+  }
+
+  return result;
+}
+
+console.log(fibonacci1(7));
+
+// GPT version fix my code
+function fibonacci2(n) {
+  if (n < 1) return [];
+  if (n == 1) return [0];
+
+  const result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    const newNumber = result[i - 2] + result[i - 1];
+    result.push(newNumber);
+  }
+
+  return result;
+}
+
+console.log(fibonacci2(7));
+
+// Codeevolution version
+function fibonacci3(n) {
+  const fib = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+}
+
+console.log(fibonacci3(7));
+
+// Big - O = O(n)
